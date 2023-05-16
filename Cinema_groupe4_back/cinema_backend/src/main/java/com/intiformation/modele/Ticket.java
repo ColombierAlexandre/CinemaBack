4d834d4c9e33f74ca57ba102b31f8b72fc7d1343
Samir31000 <131469052+Samir31000@users.noreply.github.com>
2023-05-16 11:15:43 +0200

    Update Ticket.java

--- Cinema_groupe4_back/cinema_backend/src/main/java/com/intiformation/modele/Ticket.java
+++ Cinema_groupe4_back/cinema_backend/src/main/java/com/intiformation/modele/Ticket.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Ticket {
 	
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_reservation")
 	private long id;
